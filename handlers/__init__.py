e53a995ba0b1ada2e6fef65d0da0f62670d50d6f
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-15 20:06:23 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,9 +5,13 @@ from .play import play
 from .queue import queue
 from .like import like
 from .skip import skip
+from .start import start
+from .help import help_command
 
 def register_handlers(application):
+    application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("play", play))
     application.add_handler(CommandHandler("queue", queue))
     application.add_handler(CommandHandler("like", like))
     application.add_handler(CommandHandler("skip", skip))
+    application.add_handler(CommandHandler("help", help_command))
