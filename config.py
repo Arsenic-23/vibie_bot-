0a05d88a016fb25c33ee03b3541c561c8e859899
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-15 20:07:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-BACKEND_BASE_URL
+# vibie_bot/config.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL")  # e.g. https://your-backend-url.com
